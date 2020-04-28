a9465fb58cd0c4d3c159e45c6d064b3122795814
szp <2593119609@qq.com>
2020-04-28 09:56:58 +0800

    作业8 28-苏忠沛 hefeng weather

--- chapter3/city_weather.py
+++ chapter3/city_weather.py
@@ -3,15 +3,23 @@ import requests
 
 
 class HeFeng():
-
     def __init__(self):
         self.url = "https://cdn.heweather.com/china-city-list.txt"
+        self.encoding="utf8"
+        self.pre_requests="https://free-api.heweather.net/s6/weather/now?location="
+        self.sub_requests="&key=7e71657d03174bbe9f00cb25bc873327"
+
+    def today_weather(self,city_code):
+        dict=self.get_weather(city_code)
+        print(dict["HeWeather6"][0]['now'])
 
     def get_weather(self,city_code):
-        url="https://free-api.heweather.net/s6/weather/now?location="+city_code+"&key=7e71657d03174bbe9f00cb25bc873327"
+        #url="https://free-api.heweather.net/s6/weather/now?location="+city_code+"&key=7e71657d03174bbe9f00cb25bc873327"
+        url=self.pre_requests+city_code+self.sub_requests
         info=requests.get(url)
-        info.encoding='utf8'
-        print(info.text)
+        info.encoding=self.encoding
+        return info.json()
+        #print(info.text)
 
     def get_city_code(self):
         cities = self.get_citys()
@@ -28,4 +36,7 @@ class HeFeng():
 if __name__ == '__main__':
     hefeng = HeFeng()
     codes=hefeng.get_city_code()
-    hefeng.get_weather(codes.__next__())
+    for i in range(10):
+        dict=hefeng.get_weather(codes.__next__())
+        #print(dict["HeWeather6"][0]['now'])
+        hefeng.today_weather(codes.__next__())
